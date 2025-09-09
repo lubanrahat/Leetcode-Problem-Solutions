a4345549bd9b9533f85626ae75e2244e00cbafa7
Luban Rahat <lubanrahat.dev@gmail.com>
2025-09-09 19:18:21 +0600

    Time: 0 ms (100%), Space: 20.7 MB (26.21%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int,int> count;
         int answer = 0;
-        for(int num: nums) count[num]++;
-        for(auto [key,val]: count) {
-            if(val == 1) answer = key;
+        for(int i = 0; i < nums.size(); i++) {
+            answer ^= nums[i];
         }
         return answer;
     }
